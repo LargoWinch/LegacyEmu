62ac5a40070ee618be1fcb7a9af55a8d14907a16
LargoWinch <progamer.91@mail.ru>
2013-09-29 21:59:08 +0400

    Revert "добавил логер"
    This reverts commit 65cd3b0cf865311b88ac017d10ea2d7fee24c075.

--- LegacyEmu/Login_Server/LoginServer/configuration/Config.vb
+++ LegacyEmu/Login_Server/LoginServer/configuration/Config.vb
@@ -1,32 +1,3 @@
-﻿'21:20:03 
-'29.09.2013
-'Autor: LargoWinch
+﻿Public Class Config
 
-Imports System.Text
-Friend Class Config
-
-    Public Shared SERVER_HOST As String
-    Public Shared SERVER_PORT As Integer
-    Public Shared SERVER_PORT_GAMESERVER As Integer
-
-    Public Shared DATABASE_HOST, DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD As String
-
-    Public Shared AUTO_ACCOUNTS As Boolean
-    Public Shared ShowAgreement As Boolean
-
-    Public Shared Sub load()
-        Dim server As New ConfigFile("config\server.ini")
-
-        SERVER_HOST = server.getProperty("Host", "127.0.0.1")
-        SERVER_PORT = Integer.Parse(server.getProperty("Port", "2106"))
-        SERVER_PORT_GAMESERVER = Integer.Parse(server.getProperty("GameServerPort", "2107"))
-
-        DATABASE_HOST = server.getProperty("DataBaseHost", "localhost")
-        DATABASE_NAME = server.getProperty("DataBaseName", "freya")
-        DATABASE_USER = server.getProperty("DataBaseUser", "root")
-        DATABASE_PASSWORD = server.getProperty("DataBasePassword", "default")
-
-        AUTO_ACCOUNTS = Boolean.Parse(server.getProperty("AutoAccounts", "false"))
-        ShowAgreement = Boolean.Parse(server.getProperty("ShowAgreement", "false"))
-    End Sub
 End Class
