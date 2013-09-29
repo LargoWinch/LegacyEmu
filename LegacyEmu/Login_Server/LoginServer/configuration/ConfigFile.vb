65cd3b0cf865311b88ac017d10ea2d7fee24c075
LargoWinch <progamer.91@mail.ru>
2013-09-29 21:49:31 +0400

    добавил логер

--- LegacyEmu/Login_Server/LoginServer/configuration/ConfigFile.vb
+++ LegacyEmu/Login_Server/LoginServer/configuration/ConfigFile.vb
@@ -1,3 +1,41 @@
-﻿Public Class ConfigFile
+﻿'21:38:44 
+'29.09.2013
+'Autor: LargoWinch
 
+Imports System.IO
+Friend Class ConfigFile
+    Private File As FileInfo
+    Public _topics As SortedList(Of String, String)
+
+    Public Sub New(ByVal Path As String)
+        File = New FileInfo(Path)
+        _topics = New SortedList(Of String, String)()
+        reload()
+    End Sub
+
+    Public Sub reload()
+        Dim reader As New StreamReader(File.FullName)
+        Do While Not reader.EndOfStream
+            Dim line As String = reader.ReadLine()
+            If line.Length = 0 Then
+                Continue Do
+            End If
+
+            If line.StartsWith("#") Then
+                Continue Do
+            End If
+            _topics.Add(line.Split("="c)(0), line.Split("="c)(1))
+        Loop
+    End Sub
+
+    Public Function getProperty(ByVal value As String, ByVal defaultprop As String) As String
+        Dim ret As String
+        Try
+            ret = _topics(value)
+        Catch ex As Exception
+            Return defaultprop
+        End Try
+        Return If(ret Is Nothing, defaultprop, ret)
+    End Function
 End Class
+
---
+++ LegacyEmu/Login_Server/LoginServer/util/Logger.vb
@@ -0,0 +1,55 @@
+﻿'21:47:03 
+'29.09.2013
+'Autor: LargoWinch
+
+Imports System.IO
+Friend Class Logger
+    Private Shared cf As Boolean = False
+    Private Shared tw As TextWriter
+    Private Shared name As String
+
+    Public Shared Sub warning(ByVal text As String)
+        Console.ForegroundColor = ConsoleColor.Yellow
+        Console.WriteLine(text)
+        If cf Then
+            tw.WriteLine(text)
+        End If
+        Console.ResetColor()
+    End Sub
+
+    Public Shared Sub [error](ByVal text As String)
+        Console.ForegroundColor = ConsoleColor.Red
+        Console.WriteLine(text)
+        If cf Then
+            tw.WriteLine(text)
+        End If
+        Console.ResetColor()
+    End Sub
+
+    Public Shared Sub info(ByVal text As String)
+        Console.WriteLine(text)
+        If cf Then
+            tw.WriteLine(text)
+        End If
+    End Sub
+
+    Public Shared Sub extra_info(ByVal text As String)
+        Console.ForegroundColor = ConsoleColor.DarkGray
+        Console.WriteLine(text)
+        If cf Then
+            tw.WriteLine(text)
+        End If
+        Console.ResetColor()
+    End Sub
+
+    Public Shared Sub form()
+        If Not cf Then
+            Return
+        End If
+
+        name = "log\" & Date.Now.ToString("чч-ММ-ГГ ЧЧ-мм-сс") & ".log"
+        tw = File.CreateText(name)
+        extra_info("Лог файл создан " & name)
+    End Sub
+End Class
+
