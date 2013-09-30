57d26a852c76f23113a2990c0d52384017a39388
LargoWinch <progamer.91@mail.ru>
2013-10-01 01:01:17 +0400

    rev. 0.1.8

--- LegacyEmu/Login_Server/Module1.vb
+++ LegacyEmu/Login_Server/Module1.vb
@@ -1,7 +1,45 @@
-﻿Module Module1
+﻿'00:25:40
+'01.10.2013
+'Autor: LargoWinch
+
+Imports System.Net.Sockets
+Imports System.Net
+
+Module Module1
 
     Sub Main()
-        Console.ReadLine()
+        StartProgram.getInstance()
+        Process.GetCurrentProcess().WaitForExit()
     End Sub
 
 End Module
+
+
+Friend Class StartProgram
+
+    Private Shared auth As New StartProgram()
+    Public Shared Function getInstance() As StartProgram
+        Return auth
+    End Function
+
+    Protected LoginListener As TcpListener
+
+    Public Sub New()
+        Console.Title = "Login server Lineage II"
+
+        Console.WriteLine("#======================================================================#")
+        Console.WriteLine("#                                                                      #")
+        Console.WriteLine("#$$      $$$$$    $$$$    $$$$    $$$$   $$  $$  $$$$$   $$   $$ $$  $$#")
+        Console.WriteLine("#$$      $$      $$      $$  $$  $$  $$   $$$$   $$      $$$ $$$ $$  $$#")
+        Console.WriteLine("#$$      $$$$    $$ $$$  $$$$$$  $$        $$    $$$$    $$ $ $$ $$  $$#")
+        Console.WriteLine("#$$      $$      $$  $$  $$  $$  $$  $$    $$    $$      $$   $$ $$  $$#")
+        Console.WriteLine("#$$$$$$  $$$$$    $$$$   $$  $$   $$$$     $$    $$$$$   $$   $$  $$$$ #")
+        Console.WriteLine("#----------------------------------------------------------------------#")
+        Console.WriteLine("#----------------------------------------------------------------------#")
+        Console.WriteLine("#                         VB.NET L2 Emulator                           #")
+        Console.WriteLine("#----------------------------------------------------------------------#")
+        Console.WriteLine("#              Copyright (c) 2013 Legacy Dev Team                      #")
+        Console.WriteLine("#======================================================================#")
+    End Sub
+End Class
+
