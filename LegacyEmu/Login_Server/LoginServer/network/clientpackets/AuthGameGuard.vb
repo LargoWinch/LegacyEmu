95e7feedcecf9c52427aa6e98ef7068292ba68de
LargoWinch <progamer.91@mail.ru>
2013-09-30 21:54:18 +0400

    rev. 0.1.4

---
+++ LegacyEmu/Login_Server/LoginServer/network/clientpackets/AuthGameGuard.vb
@@ -0,0 +1,17 @@
+﻿'21:30:22
+'30.09.2013
+'Autor: LargoWinch
+
+Public Class AuthGameGuard
+    Inherits L_ReceiveBasePacket
+    Public Sub New(ByVal Client As L_LoginClient, ByVal data() As Byte)
+        MyBase.makeme(Client, data)
+    End Sub
+
+    Public Overrides Sub read()
+        ' do nothing
+    End Sub
+
+    Public Overrides Sub run()
+    End Sub
+End Class
---
+++ LegacyEmu/Login_Server/LoginServer/network/game_server/SM_AUTH_GG.vb
@@ -0,0 +1,16 @@
+﻿'21:53:55
+'30.09.2013
+'Autor: LargoWinch
+
+Public Class SM_AUTH_GG
+    Inherits L_SendBasePacket
+    Public Sub New(ByVal Client As L_LoginClient)
+        MyBase.makeme(Client)
+    End Sub
+    Protected Friend Overrides Sub write()
+        writeC(&HB)
+        writeD(lc.SessionId)
+        writeB(New Byte(15) {})
+        writeB(New Byte() {&HE0, &HCD, &H13, &H4B, &H1D, &HD1, &H35, &H75, &HCB, &H83, &H44, &H35, &H24, &HEF, &H60, &H82, &HA6, &H20, &H82})
+    End Sub
+End Class
