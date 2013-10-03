5f1b01b6a575bce27c38d04150349b50412c6fb8
LargoWinch <progamer.91@mail.ru>
2013-10-03 20:38:59 +0400

    rev. 0.2.6

---
+++ GameServer/Game_Server/GameServer/base/RaceId.vb
@@ -0,0 +1,12 @@
+﻿'20:15:10
+'03.10.2013
+'Autor: LargoWinch
+
+Public Enum RaceId
+    human
+    elf
+    darkelf
+    orc
+    dwarf
+    kamael
+End Enum
---
+++ GameServer/Game_Server/GameServer/base/Sex.vb
@@ -0,0 +1,8 @@
+﻿'19:59:21
+'03.10.2013
+'Autor: LargoWinch
+
+Public Enum Sex
+    male = 0
+    female = 1
+End Enum
